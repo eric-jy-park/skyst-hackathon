5ea3f69191f487d5f92573ebb053b1de9d41f74c
eric-jy-park <2019147551@yonsei.ac.kr>
2025-05-05 21:19:45 +0900

    feat: add dummy Books CRUD endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || '',
+      port: 5432,
+      username: process.env.USERNAME || 'postgres',
+      password: process.env.PASSWORD || 'password',
+      database: process.env.DATABASE || 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+      // autoLoadEntities: process.env.NODE_ENV === 'prod' ? false : true,
+      // synchronize: process.env.NODE_ENV === 'prod' ? false : true,
+      ssl:
+        process.env.NODE_ENV === 'prod'
+          ? {
+              rejectUnauthorized: false,
+            }
+          : false,
+    }),
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
