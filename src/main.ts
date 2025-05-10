053954f4b599309043638fe98bacab03dbdfc982
eric-jy-park <2019147551@yonsei.ac.kr>
2025-05-11 06:00:22 +0900

    fix: add validation pipe with transform option

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Skyst Hackathon API')
